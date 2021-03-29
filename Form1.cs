5aef40c4d7f65c7c6d5d5e3a58e1d944257a453c
Sergio <62715874+sergioriv2@users.noreply.github.com>
2021-03-29 12:42:55 -0300

    v0.10
    Actualice los anchors para que esten centrados a la ventana del programa, tambien deshabilite los botones de maximizar de cada formulario y les asigne un minimum size

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR3TP01
+{
+    public partial class FormularioPrincipal : Form
+    {
+        public FormularioPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Ej1_Click(object sender, EventArgs e)
+        {
+            Ejercicio1 Ejer1 = new Ejercicio1();
+            Ejer1.Show();
+
+        }
+
+        private void btn_Ej2_Click(object sender, EventArgs e)
+        {
+            Ejercicio2 Ejer2 = new Ejercicio2();
+            Ejer2.Show();
+        }
+
+        private void btn_Ej3_Click(object sender, EventArgs e)
+        {
+            Ejercicio3 Ejer3 = new Ejercicio3();
+            Ejer3.Show();
+        }
+    }
+}
